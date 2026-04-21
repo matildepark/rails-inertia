b5e35bdfdc42275ff3f3f30c74839268bcfb5bb3
Matilde Park <smtp@mp9.ca>
2026-04-21 19:39:11 -0400

    app: init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+    def index
+        render inertia: 'Home', props: { message: "Hello world!"}
+    end
+end
---
+++ app/javascript/pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+    defineProps({ message: String })
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
